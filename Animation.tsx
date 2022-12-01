2456a1aa09a8f195715baed9102a94eec621e6b2
Sandevik <62107622+Sandevik@users.noreply.github.com>
2022-12-01 20:30:42 +0100

    Update Animation.tsx

--- Animation.tsx
+++ Animation.tsx
@@ -1,28 +1,26 @@
-import React, { useEffect, useState } from "react";
-import { useAnimation } from "./useAnimation";
+import React from 'react'
+import { useAnimation } from './useAnimation';
 import style from "./AnimationComponent.module.css";
 
 type AnimationType = "slideFromLeft" | "slideFromRight" | "fadeIn";
-
 export type BoxHeight = number | undefined;
 
 interface AnimationProps {
-  children: React.ReactNode;
-  animationType: AnimationType;
-  boxHeight?: BoxHeight;
+    children: React.ReactNode;
+    animationType: AnimationType;
+    boxHeight?: BoxHeight;
 }
 
-export default function Animation({ children, animationType, boxHeight }: AnimationProps) {
-  
-  const {randId, isInViewPort} = useAnimation(boxHeight)
+export default function Animation({children, animationType, boxHeight}: AnimationProps) {
+    const {inView, randId} = useAnimation(boxHeight);
 
   return (
     <div
       data-animate={randId}
       data-animation={animationType}
-      className={ isInViewPort ? `${style.animationBox} ${style.inView}` : style.animationBox}
+      className={ inView ? `${style.animationBox} ${style.inView}` : style.animationBox}
       >
       {children}
     </div>
-  );
+  )
 }
