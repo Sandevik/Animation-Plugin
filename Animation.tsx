13a31613281932e1f5a74838c23a2a0d4e6afd54
Sandevik <62107622+Sandevik@users.noreply.github.com>
2022-12-01 10:05:36 +0100

    Created Animation.tsx

---
+++ Animation.tsx
@@ -0,0 +1,28 @@
+import React, { useEffect, useState } from "react";
+import { useAnimation } from "../hooks/useAnimation";
+import style from "./AnimationComponent.module.css";
+
+type AnimationType = "slideFromLeft" | "slideFromRight" | "fadeIn";
+
+export type BoxHeight = number | undefined;
+
+interface AnimationProps {
+  children: React.ReactNode;
+  animationType: AnimationType;
+  boxHeight?: BoxHeight;
+}
+
+export default function Animation({ children, animationType, boxHeight }: AnimationProps) {
+  
+  const {randId, isInViewPort} = useAnimation(boxHeight)
+
+  return (
+    <div
+      data-animate={randId}
+      data-animation={animationType}
+      className={ isInViewPort ? `${style.animationBox} ${style.inView}` : style.animationBox}
+      >
+      {children}
+    </div>
+  );
+}
