c385c2bda5f498a5e3cb7e292c3b4715b95c5939
Sandevik <62107622+Sandevik@users.noreply.github.com>
2022-12-01 20:29:58 +0100

    Update useAnimation.tsx Final

--- useAnimation.tsx
+++ useAnimation.tsx
@@ -1,41 +1,34 @@
-import { useEffect, useState } from "react";
-
-import type {BoxHeight} from "./Animation"
-
-
+import { useEffect, useState } from "react"
+import type { BoxHeight } from "../components/Animation";
 
 export const useAnimation = (boxHeight: BoxHeight) => {
-    const [isInViewPort, setIsInViewPort] = useState<boolean>(false);
-  const [randId, setRandId] = useState<number>(0);
+    const[inView, setInView] = useState<boolean>(false);
+    const[randId, setRandId] = useState<number>(0);
+    function isElementInViewport(el: Element | null) {
+        let rect = el
+          ? el.getBoundingClientRect()
+          : { bottom: 0, left: 0, right: 0, top: 0 };
+      
+        return (
+          rect.bottom > 0 &&
+          rect.top < (window.innerHeight || document.documentElement.clientHeight)
+        );
+    }
+    function generateRandomId (){
+        return Math.random()*10000;
+    }
+    useEffect(()=>{setRandId(generateRandomId())},[])
+    useEffect(()=>{
+        const animate = document.querySelector(`[data-animate="${randId}"]`) as HTMLElement | null;
+        animate?.style.setProperty("--Height", `${boxHeight ? boxHeight : "200"}px`);
+        const scrollHandler = () => {
+            if (isElementInViewport(animate)) {
+                setInView(true);
+            }
+        };
+        window.addEventListener("scroll", scrollHandler);
+        return () => window.removeEventListener("scroll", scrollHandler);
+    },[inView, randId])
 
-  function isElementInViewport(el: Element | null) {
-    let rect = el
-      ? el.getBoundingClientRect()
-      : { bottom: 0, left: 0, right: 0, top: 0 };
-
-    return (
-      rect.bottom > 0 &&
-      rect.top < (window.innerHeight || document.documentElement.clientHeight)
-    );
-  }
-
-  const generateRandomId = (): void =>{
-    setRandId((Math.random()*10000)) 
-  }
-
-  useEffect(() => {
-    generateRandomId();
-    const animate = document.querySelector(`[data-animate="${randId}"]`) as HTMLElement | null;
-    animate?.style.setProperty("--Height", `${boxHeight ? boxHeight : "200"}px`);
-    console.log(animate);
-    const scrollHandler = () => {
-      if (isElementInViewport(animate)) setIsInViewPort(true);
-    };
-    window.addEventListener("scroll", scrollHandler);
-    return () => window.removeEventListener("scroll", scrollHandler);
-  }, [boxHeight, isInViewPort]);
-
-
-  return {randId, isInViewPort}
-
+    return {inView, randId}
 }
